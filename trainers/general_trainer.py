5acdceb0861ecbf072c0adf388e60109162c2711
jaduranfernandez <jaduranfernandez@outlook.es>
2023-10-09 20:05:48 +0200

    General trainer added for pytorch gans

--- trainers/general_trainer.py
+++ trainers/general_trainer.py
@@ -2,62 +2,54 @@ import math
 
 import torch
 import torch.nn as nn
-
+import numpy as np
 
 class Trainer:
-    def __init__(self, model, criterion, optimizer):
-        self.GAN = model
-        self.criterion = criterion
-        self.optimizer = optimizer
+    def __init__(self, generator, discriminator, generator_loss, discriminator_loss, generator_optimizer, discriminator_optimizer):
+        self.generator = generator
+        self.generator_loss = generator_loss
+        self.generator_optimizer = generator_optimizer
+        self.discriminator = discriminator
+        self.discriminator_loss = discriminator_loss
+        self.discriminator_optimizer = discriminator_optimizer
 
-    def train_general(input_length: int = 10, batch_size: int = 16, training_steps: int = 500):
+
+    def train_general(self, train_data: np.ndarray = [], input_length: int = 10, batch_size: int = 16, n_iterations: int = 500):
         
-        pass
-
-    def train(max_int: int = 128, batch_size: int = 16, training_steps: int = 500):
-        input_length = int(math.log(max_int, 2))
-
-        # Models
-        generator = Generator(input_length)
-        discriminator = Discriminator(input_length)
-
-        # Optimizers
-        generator_optimizer = torch.optim.Adam(generator.parameters(), lr=0.001)
-        discriminator_optimizer = torch.optim.Adam(discriminator.parameters(), lr=0.001)
-
-        # loss
-        loss = nn.BCELoss()
-
-        for i in range(training_steps):
+        for i in range(n_iterations):
             # zero the gradients on each iteration
-            generator_optimizer.zero_grad()
+            self.generator_optimizer.zero_grad()
 
             # Create noisy input for generator
-            # Need float type instead of int
-            noise = torch.randint(0, 2, size=(batch_size, input_length)).float()
-            generated_data = generator(noise)
+            noise = np.random.normal(0,1,[batch_size,input_length])
+            fake_data = self.generator(noise)
 
-            # Generate examples of even real data
-            true_labels, true_data = generate_even_data(max_int, batch_size=batch_size)
+            # Generate examples of real data
+            idx = np.random.randint(low=0, high=train_data.shape[0],size=batch_size)
+            true_data = train_data[idx]
+            true_labels = np.ones(batch_size)
             true_labels = torch.tensor(true_labels).float()
             true_data = torch.tensor(true_data).float()
+
 
             # Train the generator
             # We invert the labels here and don't train the discriminator because we want the generator
             # to make things the discriminator classifies as true.
-            generator_discriminator_out = discriminator(generated_data)
-            generator_loss = loss(generator_discriminator_out, true_labels)
+            generator_discriminator_out = self.discriminator(fake_data)
+            generator_loss = self.generator_loss(generator_discriminator_out, true_labels)
             generator_loss.backward()
-            generator_optimizer.step()
+            self.generator_optimizer.step()
 
             # Train the discriminator on the true/generated data
-            discriminator_optimizer.zero_grad()
-            true_discriminator_out = discriminator(true_data)
-            true_discriminator_loss = loss(true_discriminator_out, true_labels)
+            self.discriminator_optimizer.zero_grad()
+            true_discriminator_out = self.discriminator(true_data)
+            true_discriminator_loss = self.discriminator_loss(true_discriminator_out, true_labels)
 
-            # add .detach() here think about this
-            generator_discriminator_out = discriminator(generated_data.detach())
-            generator_discriminator_loss = loss(generator_discriminator_out, torch.zeros(batch_size))
+            generator_discriminator_out = self.discriminator(fake_data.detach())
+            generator_discriminator_loss = self.generator_loss(generator_discriminator_out, torch.zeros(batch_size))
             discriminator_loss = (true_discriminator_loss + generator_discriminator_loss) / 2
             discriminator_loss.backward()
-            discriminator_optimizer.step()
+            self.discriminator_optimizer.step()
+
+
+
