53eb1dd5b644503dfd5828e6c47e09a17d4dc411
jaduranfernandez <jaduranfernandez@outlook.es>
2023-10-18 20:56:36 +0200

    DataLoader functionality for faces images

--- data_manipulation/faces.py
+++ data_manipulation/faces.py
@@ -1,6 +1,37 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 from keras import layers, Sequential
+import torch
+import os
+from PIL import Image
+import torch
+from torchvision import transforms
+from torch.utils.data import Dataset
+
+
+class FacesDataset(Dataset):
+    def __init__(self, data_dir, transform=None):
+        self.data_dir = data_dir
+        self.image_paths = [os.path.join(data_dir, filename) for filename in os.listdir(data_dir)]
+        if(transform==None):
+            transform = transforms.Compose([
+                transforms.Resize((128, 128)),  # Resize images to 128x128
+                transforms.ToTensor(),  # Convert images to tensors
+                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # Normalize to [-1, 1]
+            ])
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        image_path = self.image_paths[idx]
+        image = Image.open(image_path)
+        if self.transform:
+            image = self.transform(image)
+        return image
+
+
 
 
 def create_dataset(folder_path, file_name, data_path, first_image, last_image):
