4c8367c5205aa5f2a82e02bae0f97564e2b0db2d
jaduranfernandez <jaduranfernandez@outlook.es>
2023-10-19 16:44:38 +0200

    Plot fake images added

--- data_manipulation/faces.py
+++ data_manipulation/faces.py
@@ -1,10 +1,6 @@
 import matplotlib.pyplot as plt 
-import numpy as np
-from keras import layers, Sequential
-import torch
 import os
 from PIL import Image
-import torch
 from torchvision import transforms
 from torch.utils.data import Dataset
 
@@ -30,25 +26,4 @@ class FacesDataset(Dataset):
         if self.transform:
             image = self.transform(image)
         return image
-
-
-
-
-def create_dataset(folder_path, file_name, data_path, first_image, last_image):
-    
-    dimensions = [128,128,3]
-    values = np.zeros((last_image - first_image+1, dimensions[0],dimensions[1],dimensions[2]))
-    # Resizing and rescaling
-    resize_and_scale = Sequential()
-    resize_and_scale.add(layers.Resizing(dimensions[0], dimensions[1]))
-    resize_and_scale.add(layers.Rescaling(1./255))
-
-
-    for it in range(first_image, last_image+1):
-        filename = str(it).zfill(6) + ".jpg"
-        image = plt.imread(data_path + filename)
-        new_image = resize_and_scale(image)
-        new_image = (2*new_image) - 1
-        values[it-1,:] = new_image
-    np.save(folder_path + file_name, values)
 
